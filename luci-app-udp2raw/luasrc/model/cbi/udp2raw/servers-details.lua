866ed622a5c3393352c70eb28d1bdb2a369eac70
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2023-09-18 07:41:55 -0700

    Update servers-details.lua

--- luci-app-udp2raw/luasrc/model/cbi/udp2raw/servers-details.lua
+++ luci-app-udp2raw/luasrc/model/cbi/udp2raw/servers-details.lua
@@ -20,23 +20,6 @@ local auth_modes = {
 	"none",
 }
 
-local sock_buf = {
-	"10",
-	"128",
-	"256",
-	"512",
-	"1024",
-	"2048",
-	"3072",
-	"4096",
-	"5120",
-	"6144",
-	"7168",
-	"8192",
-	"9216",
-	"10240",	
-}
-
 m = Map("udp2raw", "%s - %s" %{translate("udp2raw-tunnel"), translate("Edit Server")})
 m.redirect = luci.dispatcher.build_url("admin/services/udp2raw/servers")
 m.sid = sid
@@ -88,7 +71,7 @@ o = s:option(Flag, "fix_gro", translate("Fix Gro"), translate("Fix Huge packet w
 o.default = "0"
 
 o = s:option(ListValue, "sock_buf", translate("Sock Buff"), translate("buf size for socket,>=10 and <=10240,unit:kbyte,default:1024"))
-for _, v in ipairs(sock_buf) do o:value(v, v:lower()) end
+o.datatype = "range(10,10240)"
 o.default = "1024"
 
 o = s:option(Flag, "auto_rule", translate("Auto Rule"), translate("Auto add (and delete) iptables rule."))
